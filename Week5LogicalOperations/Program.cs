7e1663fa08128b62613cd2f1af0067cdf611a020
MZadonski <maria.zadons@tktk.ee>
2023-05-07 00:02:29 +0300

    create new app

---
+++ Week5LogicalOperations/Program.cs
@@ -0,0 +1,13 @@
+﻿//AND - &&
+
+//true && true --> true
+//true && false --> false
+//fals && true --> false
+//fals && false --> false
+
+//OR - || (pipes)
+
+//true || true --> true
+//true || false --> true
+//false || true --> true
+//false || false --> false
